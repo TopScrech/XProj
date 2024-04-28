10a03358d84d6a119762bd8e7a0b2cb999ac1336
TopScrech <sergei_saliukov@icloud.com>
2024-04-28 09:43:06 +0200

    Initial Commit

---
+++ XProj/XProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  XProjApp.swift
+//  XProj
+//
+//  Created by Sergei Saliukov on 28/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct XProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
