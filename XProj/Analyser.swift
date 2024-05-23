0b381d3f990be30a9ae2bd56212f9ca3d4d6bef1
TopScrech <sergei_saliukov@icloud.com>
2024-05-23 03:05:51 +0200

    Tested

--- XProj/Analyser.swift
+++ XProj/Analyser.swift
@@ -5,44 +5,166 @@ extension ProjectListVM {
         let startTime = CFAbsoluteTimeGetCurrent()
         
         var folderContents = [String: [String]?]()
+        var totalFiles = 0
         
         for folder in folderPaths {
-            let url = URL(fileURLWithPath: folder)
-            do {
-                let files = try FileManager.default.contentsOfDirectory(atPath: url.path)
+            if let files = listFilesRecursively(in: folder) {
                 folderContents[folder] = files
-            } catch {
+                totalFiles += files.count
+            } else {
                 folderContents[folder] = nil
             }
         }
         
         let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-        print("Single-threaded listFilesInFolders completed in \(timeElapsed) seconds")
+        print(String(format: "Single-threaded scanned \(folderContents.count) folders in %.3f seconds", timeElapsed))
+        print("Total files found: \(totalFiles)")
         
         return folderContents
+    }
+    
+    func listFilesRecursively(in folder: String) -> [String]? {
+        var allFiles = [String]()
+        
+        do {
+            let filesAndFolders = try FileManager.default.contentsOfDirectory(atPath: folder)
+            for item in filesAndFolders {
+                let fullPath = (folder as NSString).appendingPathComponent(item)
+                var isDirectory: ObjCBool = false
+                if FileManager.default.fileExists(atPath: fullPath, isDirectory: &isDirectory) {
+                    if isDirectory.boolValue {
+                        if let nestedFiles = listFilesRecursively(in: fullPath) {
+                            allFiles.append(contentsOf: nestedFiles)
+                        }
+                    } else {
+                        allFiles.append(fullPath)
+                    }
+                }
+            }
+        } catch {
+            return nil
+        }
+        
+        return allFiles
     }
     
     func countFilesInFoldersSingleThread(folderPaths: [String]) -> [String: Int?] {
         let startTime = CFAbsoluteTimeGetCurrent()
         
         var folderFileCounts = [String: Int?]()
+        var totalFiles = 0
         
         for folder in folderPaths {
-            let url = URL(fileURLWithPath: folder)
-            do {
-                let files = try FileManager.default.contentsOfDirectory(atPath: url.path)
-                folderFileCounts[folder] = files.count
-            } catch {
+            if let count = countFilesRecursively(in: folder) {
+                folderFileCounts[folder] = count
+                totalFiles += count
+            } else {
                 folderFileCounts[folder] = nil
             }
         }
         
         let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-        print("Single-threaded countFilesInFolders completed in \(timeElapsed) seconds")
+        print(String(format: "Single-threaded scanned \(folderFileCounts.count) folders in %.3f seconds", timeElapsed))
+        print("Total files found: \(totalFiles)")
         
         return folderFileCounts
     }
+    
+    func countFilesRecursively(in folder: String) -> Int? {
+        var fileCount = 0
+        
+        do {
+            let filesAndFolders = try FileManager.default.contentsOfDirectory(atPath: folder)
+            for item in filesAndFolders {
+                let fullPath = (folder as NSString).appendingPathComponent(item)
+                var isDirectory: ObjCBool = false
+                if FileManager.default.fileExists(atPath: fullPath, isDirectory: &isDirectory) {
+                    if isDirectory.boolValue {
+                        if let nestedCount = countFilesRecursively(in: fullPath) {
+                            fileCount += nestedCount
+                        }
+                    } else {
+                        fileCount += 1
+                    }
+                }
+            }
+        } catch {
+            return nil
+        }
+        
+        return fileCount
+    }
 }
+
+//extension ProjectListVM {
+//    func listFilesInFoldersMultiThread(folderPaths: [String], completion: @escaping ([String: [String]?]) -> Void) {
+//        let startTime = CFAbsoluteTimeGetCurrent()
+//        
+//        var folderContents = [String: [String]?]()
+//        let dispatchGroup = DispatchGroup()
+//        let lock = NSLock()
+//        var totalFiles = 0
+//        
+//        for folder in folderPaths {
+//            dispatchGroup.enter()
+//            DispatchQueue.global().async {
+//                let files = self.listFilesRecursively(in: folder)
+//                DispatchQueue.main.async {
+//                    if let files = files {
+//                        lock.lock()
+//                        folderContents[folder] = files
+//                        totalFiles += files.count
+//                        lock.unlock()
+//                    } else {
+//                        folderContents[folder] = nil
+//                    }
+//                    dispatchGroup.leave()
+//                }
+//            }
+//        }
+//        
+//        dispatchGroup.notify(queue: .main) {
+//            let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+//            print(String(format: "Multi-threaded scanned \(folderContents.count) folders in %.3f seconds", timeElapsed))
+//            print("Total files found: \(totalFiles)")
+//            completion(folderContents)
+//        }
+//    }
+//    
+//    func countFilesInFoldersMultiThread(folderPaths: [String], completion: @escaping ([String: Int?]) -> Void) {
+//        let startTime = CFAbsoluteTimeGetCurrent()
+//        
+//        var folderFileCounts = [String: Int?]()
+//        let dispatchGroup = DispatchGroup()
+//        let lock = NSLock()
+//        var totalFiles = 0
+//        
+//        for folder in folderPaths {
+//            dispatchGroup.enter()
+//            DispatchQueue.global().async {
+//                let count = self.countFilesRecursively(in: folder)
+//                DispatchQueue.main.async {
+//                    if let count = count {
+//                        lock.lock()
+//                        folderFileCounts[folder] = count
+//                        totalFiles += count
+//                        lock.unlock()
+//                    } else {
+//                        folderFileCounts[folder] = nil
+//                    }
+//                    dispatchGroup.leave()
+//                }
+//            }
+//        }
+//        
+//        dispatchGroup.notify(queue: .main) {
+//            let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+//            print(String(format: "Multi-threaded scanned \(folderFileCounts.count) folders in %.3f seconds", timeElapsed))
+//            print("Total files found: \(totalFiles)")
+//            completion(folderFileCounts)
+//        }
+//    }
+//}
 
 extension ProjectListVM {
     func listFilesInFoldersMultiThread(folderPaths: [String], completion: @escaping ([String: [String]?]) -> Void) {
@@ -50,29 +172,31 @@ extension ProjectListVM {
         
         var folderContents = [String: [String]?]()
         let dispatchGroup = DispatchGroup()
+        let lock = NSLock()
+        var totalFiles = 0
         
         for folder in folderPaths {
             dispatchGroup.enter()
             DispatchQueue.global().async {
-                let url = URL(fileURLWithPath: folder)
-                do {
-                    let files = try FileManager.default.contentsOfDirectory(atPath: url.path)
-                    DispatchQueue.main.async {
+                let files = self.listFilesRecursively(in: folder)
+                DispatchQueue.main.async {
+                    if let files = files {
+                        lock.lock()
                         folderContents[folder] = files
-                        dispatchGroup.leave()
-                    }
-                } catch {
-                    DispatchQueue.main.async {
+                        totalFiles += files.count
+                        lock.unlock()
+                    } else {
                         folderContents[folder] = nil
-                        dispatchGroup.leave()
                     }
+                    dispatchGroup.leave()
                 }
             }
         }
         
         dispatchGroup.notify(queue: .main) {
             let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-            print("Multi-threaded listFilesInFolders completed in \(timeElapsed) seconds")
+            print(String(format: "Multi-threaded scanned \(folderContents.count) folders in %.3f seconds", timeElapsed))
+            print("Total files found: \(totalFiles)")
             completion(folderContents)
         }
     }
@@ -82,29 +206,31 @@ extension ProjectListVM {
         
         var folderFileCounts = [String: Int?]()
         let dispatchGroup = DispatchGroup()
+        let lock = NSLock()
+        var totalFiles = 0
         
         for folder in folderPaths {
             dispatchGroup.enter()
             DispatchQueue.global().async {
-                let url = URL(fileURLWithPath: folder)
-                do {
-                    let files = try FileManager.default.contentsOfDirectory(atPath: url.path)
-                    DispatchQueue.main.async {
-                        folderFileCounts[folder] = files.count
-                        dispatchGroup.leave()
-                    }
-                } catch {
-                    DispatchQueue.main.async {
+                let count = self.countFilesRecursively(in: folder)
+                DispatchQueue.main.async {
+                    if let count = count {
+                        lock.lock()
+                        folderFileCounts[folder] = count
+                        totalFiles += count
+                        lock.unlock()
+                    } else {
                         folderFileCounts[folder] = nil
-                        dispatchGroup.leave()
                     }
+                    dispatchGroup.leave()
                 }
             }
         }
         
         dispatchGroup.notify(queue: .main) {
             let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-            print("Multi-threaded countFilesInFolders completed in \(timeElapsed) seconds")
+            print(String(format: "Multi-threaded scanned \(folderFileCounts.count) folders in %.3f seconds", timeElapsed))
+            print("Total files found: \(totalFiles)")
             completion(folderFileCounts)
         }
     }
