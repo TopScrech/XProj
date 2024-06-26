a5a7f370038c291400dcd0423ea833f6415a327b
TopScrech <sergei_saliukov@icloud.com>
2024-06-26 15:24:16 +0200

    Improved Search Suggestions

--- XProj/Elements/Search Suggestions.swift
+++ XProj/Elements/Search Suggestions.swift
@@ -3,8 +3,18 @@ import SwiftUI
 struct SearchSuggestions: View {
     @Environment(ProjectListVM.self) private var vm
     
+    private var suggestedProjects: [Project] {
+        guard !vm.searchPrompt.isEmpty else {
+            return vm.lastOpenedProjects
+        }
+        
+        return vm.lastOpenedProjects.filter {
+            $0.name.contains(vm.searchPrompt)
+        }
+    }
+    
     var body: some View {
-        ForEach(vm.lastOpenedProjects) { proj in
+        ForEach(suggestedProjects) { proj in
             Button {
                 vm.searchPrompt = proj.name
             } label: {
