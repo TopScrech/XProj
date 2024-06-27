e232d5b195487fe873853dc9c777d2d5a7786dbe
TopScrech <sergei_saliukov@icloud.com>
2024-06-27 13:51:06 +0200

    -

--- XProj/Project List.swift
+++ XProj/Project List.swift
@@ -28,37 +28,41 @@ struct ProjectList: View {
         }
         .toolbar {
             ToolbarItemGroup {
-                Button("listFilesInFoldersSingleThread") {
-                    let test = vm.listFilesInFoldersSingleThread(folderPaths: vm.projects.map(\.path))
-                }
-                
-                Button("countFilesInFoldersMultiThread") {
-                    vm.countFilesInFoldersMultiThread(folderPaths: vm.projects.map(\.path)) { _ in
-                        
+                Menu {
+                    Button("listFilesInFoldersSingleThread") {
+                        let test = vm.listFilesInFoldersSingleThread(folderPaths: vm.projects.map(\.path))
                     }
-                }
-                
-                Button("countFilesRecursively") {
-                    DispatchQueue.global().async {
-                        let start = DispatchTime.now()
-                        
-                        let test = vm.countFilesRecursively("/Users/topscrech/Projects")
-                        print(test)
-                        
-                        DispatchQueue.main.async {
-                            let finish = DispatchTime.now()
-                            let timeElapsed = finish.uptimeNanoseconds - start.uptimeNanoseconds
-                            let timeElapsedInSeconds = Double(timeElapsed) / 1_000_000_000
+                    
+                    Button("countFilesInFoldersMultiThread") {
+                        vm.countFilesInFoldersMultiThread(folderPaths: vm.projects.map(\.path)) { _ in
                             
-                            print("Time elapsed: \(timeElapsedInSeconds) seconds")
                         }
                     }
-                }
-                
-                Button("countFilesInFoldersMultiThread") {
-                    vm.countFilesInFoldersMultiThread(folderPaths: vm.projects.map(\.path)) { _ in
-                        
+                    
+                    Button("countFilesRecursively") {
+                        DispatchQueue.global().async {
+                            let start = DispatchTime.now()
+                            
+                            let test = vm.countFilesRecursively("/Users/topscrech/Projects")
+                            print(test)
+                            
+                            DispatchQueue.main.async {
+                                let finish = DispatchTime.now()
+                                let timeElapsed = finish.uptimeNanoseconds - start.uptimeNanoseconds
+                                let timeElapsedInSeconds = Double(timeElapsed) / 1_000_000_000
+                                
+                                print("Time elapsed: \(timeElapsedInSeconds) seconds")
+                            }
+                        }
                     }
+                    
+                    Button("countFilesInFoldersMultiThread") {
+                        vm.countFilesInFoldersMultiThread(folderPaths: vm.projects.map(\.path)) { _ in
+                            
+                        }
+                    }
+                } label: {
+                    Text("Functions")
                 }
             }
         }
