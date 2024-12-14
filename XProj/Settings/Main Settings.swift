5518549c954b1f19b71297db8416b6005e100a43
TopScrech <sergei_saliukov@icloud.com>
2024-12-14 12:02:41 +0100

    -

--- XProj/Settings/Main Settings.swift
+++ XProj/Settings/Main Settings.swift
@@ -4,39 +4,26 @@ struct MainSettings: View {
     @Environment(ProjectListVM.self) private var vm
     
     var body: some View {
-        VStack {
-            Button("Choose projects folder") {
+        List {
+            Button("Change projects folder") {
                 vm.openFolderPicker()
             }
             
             Button("Examples") {
                 downloadExamples()
             }
-#if DEBUG
-            Button("Cause a fatal error") {
-                fatalError("Crash button pressed.")
-            }
-            
-            Button("Exit") {
-                exit(0)
-            }
-            
-            Button("Crash") {
-                let _ = URL(string: "")!
-            }
-#endif
         }
         .frame(width: 200, height: 300)
     }
     
     private func downloadExamples() {
         guard let sourceURL = Bundle.main.url(forResource: "Examples", withExtension: "zip") else {
-            print("Examples.zip not found in the main bundle.")
+            print("Examples.zip not found in the main bundle")
             return
         }
         
         guard let downloadsURL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first else {
-            print("Unable to locate the Downloads folder.")
+            print("Unable to locate the Downloads folder")
             return
         }
         
@@ -44,7 +31,7 @@ struct MainSettings: View {
         
         do {
             try FileManager.default.copyItem(at: sourceURL, to: destinationURL)
-            print("Examples.zip successfully downloaded to the Downloads folder.")
+            print("Examples.zip successfully downloaded to the Downloads folder")
         } catch {
             print("Error downloading Examples.zip: \(error.localizedDescription)")
         }
