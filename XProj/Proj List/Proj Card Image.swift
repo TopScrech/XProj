792b6c79aa0d125fcc468a7dd19034279ee7df14
TopScrech <sergei_saliukov@icloud.com>
2024-12-17 15:20:21 +0100

    -

--- XProj/Proj List/Proj Card Image.swift
+++ XProj/Proj List/Proj Card Image.swift
@@ -8,29 +8,28 @@ struct ProjCardImage: View {
     }
     
     var body: some View {
-        if let path = proj.projIcon(),
-           let nsImage = NSImage(contentsOf: URL(fileURLWithPath: path)) {
-            Image(nsImage: nsImage)
-                .resizable()
-                .frame(width: 32, height: 32)
-                .clipShape(.rect(cornerRadius: 5))
-            
-        } else if proj.type == .proj {
-            Image(.projIcon)
-                .resizable()
-                .frame(width: 32, height: 32)
-            
-        } else if proj.type == .vapor {
-            Image(.vapor)
-                .resizable()
-                .frame(width: 32, height: 32)
-            
-        } else {
-            Image(systemName: proj.icon)
-                .title()
-                .foregroundStyle(proj.iconColor.gradient)
-                .frame(width: 32)
+        Group {
+            if let path = proj.projIcon(),
+               let nsImage = NSImage(contentsOf: URL(fileURLWithPath: path)) {
+                Image(nsImage: nsImage)
+                    .resizable()
+                    .clipShape(.rect(cornerRadius: 5))
+                
+            } else if proj.type == .proj {
+                Image(.projIcon)
+                    .resizable()
+                
+            } else if proj.type == .vapor {
+                Image(.vapor)
+                    .resizable()
+                
+            } else {
+                Image(systemName: proj.icon)
+                    .title()
+                    .foregroundStyle(proj.iconColor.gradient)
+            }
         }
+        .frame(width: 32, height: 32)
     }
 }
 
