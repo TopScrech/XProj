c57f4fab8726fed3622f45baa982fa8bef18f53a
TopScrech <sergei_saliukov@icloud.com>
2024-12-21 09:53:39 +0100

    Added a few more properties

--- XProj/New Nav/Models/DataModel.swift
+++ XProj/New Nav/Models/DataModel.swift
@@ -22,7 +22,7 @@ final class DataModel {
     private init(contentsOf url: URL, options: Data.ReadingOptions) throws {
         let data = try Data(contentsOf: url, options: options)
         let recipes = getFolders()
-//        let recipes = try JSONDecoder().decode([Recipe].self, from: data)
+        //        let recipes = try JSONDecoder().decode([Recipe].self, from: data)
         
         recipesById = Dictionary(uniqueKeysWithValues: recipes.map { recipe in
             (recipe.id, recipe)
@@ -54,12 +54,12 @@ final class DataModel {
         }
     }
     
-//    /// The related recipes for a given recipe, sorted by name
-//    func recipes(relatedTo recipe: Recipe) -> [Recipe] {
-//        recipes.filter {
-//            recipe.related.contains($0.id)
-//        }
-//    }
+    //    /// The related recipes for a given recipe, sorted by name
+    //    func recipes(relatedTo recipe: Recipe) -> [Recipe] {
+    //        recipes.filter {
+    //            recipe.related.contains($0.id)
+    //        }
+    //    }
     
     /// Accesses the recipe associated with the given unique identifier
     /// if the identifier is tracked by the data model; otherwise, returns `nil`
@@ -69,146 +69,197 @@ final class DataModel {
 }
 
 //extension DataModel {
-    func getFolders() -> [Recipe] {
-        let startTime = CFAbsoluteTimeGetCurrent()
-        restoreAccessToFolderOld()
-//        projects = []
-        
-        guard let url = FolderAccessManager.shared.restoreAccessToFolder(udKey) else {
-            print("Unable to restore access to the folder. Please select a new folder")
-            return []
-        }
-        
-        defer {
-            url.stopAccessingSecurityScopedResource()
-        }
-        
-        do {
-            return try processPath(url.path)
-        } catch {
-            print("Error processing path: \(error.localizedDescription)")
-        }
-        
-        let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
-        print("Time elapsed for processing path: \(String(format: "%.3f", timeElapsed)) seconds")
-        
+func getFolders() -> [Recipe] {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    restoreAccessToFolderOld()
+    //        projects = []
+    
+    guard let url = FolderAccessManager.shared.restoreAccessToFolder(udKey) else {
+        print("Unable to restore access to the folder. Please select a new folder")
         return []
     }
     
-    private func processPath(_ path: String) throws -> [Recipe] {
-        let projects = try fm.contentsOfDirectory(atPath: path)
-        
-        var found: [Recipe] = []
-        
+    defer {
+        url.stopAccessingSecurityScopedResource()
+    }
+    
+    do {
+        return try processPath(url.path)
+    } catch {
+        print("Error processing path: \(error.localizedDescription)")
+    }
+    
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    print("Time elapsed for processing path: \(String(format: "%.3f", timeElapsed)) seconds")
+    
+    return []
+}
+
+private func processPath(_ path: String) throws -> [Recipe] {
+    let projects = try fm.contentsOfDirectory(atPath: path)
+    
+    var found: [Recipe] = []
+    
 #warning("10")
-        for proj in projects.prefix(upTo: 10) {
-            if let projj = try processProj(proj, at: path) {
-                found.append(projj)
-            }
+    for proj in projects.prefix(upTo: 10) {
+        if let projj = try processProj(proj, at: path) {
+            found.append(projj)
         }
-        
-        return found
     }
     
-    private func processProj(_ proj: String, at path: String) throws -> Recipe? {
-        var name = proj
-        
-        let projPath = path + "/" + name
-        let attributes = try fm.attributesOfItem(atPath: projPath)
-        
-//        let typeAttribute = attributes[.type] as? String ?? "Other"
-        
-        if let isHidden = attributes[.extensionHidden] as? Bool, isHidden {
-            return nil
-        }
-        
-        if name == ".git" || name == ".build" {
-            return nil
-        }
-        
-//        let fileType: ProjType
-        
-//#warning("Workspaces are not fully supported")
-//        if hasFile(ofType: "xcodeproj", at: projPath) {
-//            if hasFile(ofType: "xcworkspace", at: projPath) {
-//                fileType = .workspace
-//            } else {
-//                fileType = .proj
-//            }
-//
-//        } else if hasSwiftPackage(projPath) {
-//            fileType = hasVapor(projPath) ? .vapor : .package
-//
-//            //            if isVapor(proj, path) {
-//            //                fileType = .vapor
-//            //            } else {
-//            //                fileType = .package
-//            //            }
-//
-//        } else if name.contains(".playground") {
-//            fileType = .playground
-            name = name.replacingOccurrences(of: ".playground", with: "")
-//
-//        } else {
-//            switch typeAttribute {
-//            case "NSFileTypeDirectory":
-//                try processPath(projPath)
-//                return
-//
-//            default:
-//                return
-//            }
-//        }
-        
-//        guard let openedAt = lastAccessDate(projPath) else {
-//            return
-//        }
-//
-//        let modifiedAt = attributes[.modificationDate] as? Date
-//        let createdAt = attributes[.creationDate] as? Date
-        print(name)
-        
-//        projects.append(
-            return Recipe(
-                id: name,
-                name: name,
-                category: .allItems
-//                path: projPath,
-//                type: fileType,
-//                openedAt: openedAt,
-//                modifiedAt: modifiedAt,
-//                createdAt: createdAt,
-//                attributes: attributes
-            )
-//        )
+    return found
+}
+
+private func processProj(_ proj: String, at path: String) throws -> Recipe? {
+    var name = proj
+    
+    let projPath = path + "/" + name
+    let attributes = try fm.attributesOfItem(atPath: projPath)
+    
+    let typeAttribute = attributes[.type] as? String ?? "Other"
+    
+    if let isHidden = attributes[.extensionHidden] as? Bool, isHidden {
+        return nil
     }
     
-    func restoreAccessToFolderOld() {
-        guard let bookmarkData = UserDefaults.standard.data(forKey: udKey) else {
-            return
-        }
-        
-        var isStale = false
-        
-        do {
-            let url = try URL(
-                resolvingBookmarkData: bookmarkData,
-                options: .withSecurityScope,
-                relativeTo: nil,
-                bookmarkDataIsStale: &isStale
-            )
-            
-            projectsFolder = url.path
-            
-            if url.startAccessingSecurityScopedResource() {
-                // You can now access the folder here
-            }
-            
-            if isStale {
-                print("Bookmark data is stale. Need to reselect folder for a new bookmark")
-            }
-        } catch {
-            print("Error restoring access: \(error)")
-        }
+    if name == ".git" || name == ".build" {
+        return nil
     }
+    
+    let fileType: ProjType
+    
+#warning("Workspaces are not fully supported")
+    if hasFile(ofType: "xcodeproj", at: projPath) {
+        if hasFile(ofType: "xcworkspace", at: projPath) {
+            fileType = .workspace
+        } else {
+            fileType = .proj
+        }
+        
+    } else if hasSwiftPackage(projPath) {
+        fileType = hasVapor(projPath) ? .vapor : .package
+        
+    } else if name.contains(".playground") {
+        fileType = .playground
+        name = name.replacingOccurrences(of: ".playground", with: "")
+        
+    } else {
+        fileType = .unknown
+        //        switch typeAttribute {
+        //        case "NSFileTypeDirectory":
+#warning("SUBFOLDERS")
+        ////            try processPath(projPath)
+        ////            return nil
+        //
+        //        default:
+        //            return nil
+        //        }
+    }
+    
+    guard let openedAt = lastAccessDate(projPath) else {
+        return nil
+    }
+    
+    let modifiedAt = attributes[.modificationDate] as? Date
+    let createdAt = attributes[.creationDate] as? Date
+    
+    //        projects.append(
+    return Recipe(
+        id: name,
+        name: name,
+        type: fileType,
+        category: .allItems,
+        //                path: projPath,
+        //                type: fileType,
+        openedAt: openedAt,
+        modifiedAt: modifiedAt,
+        createdAt: createdAt
+        //                attributes: attributes
+    )
+    //        )
+}
+
+/*private */func hasFile(ofType type: String, at path: String) -> Bool {
+    do {
+        let contents = try fm.contentsOfDirectory(atPath: path)
+        
+        return contents.contains {
+            $0.hasSuffix("." + type)
+        }
+    } catch {
+        return false
+    }
+}
+
+/*private */func hasSwiftPackage(_ path: String) -> Bool {
+    do {
+        let contents = try fm.contentsOfDirectory(atPath: path)
+        
+        return contents.contains("Package.swift")
+    } catch {
+        return false
+    }
+}
+/*private*/ func hasVapor( _ path: String) -> Bool {
+    let resolvedPath = path + "/Package.resolved"
+    
+    guard fm.fileExists(atPath: resolvedPath) else {
+        return false
+    }
+    
+    let vaporUrl = "https://github.com/vapor/vapor.git"
+    
+    do {
+        let fileContents = try String(contentsOfFile: resolvedPath, encoding: .utf8)
+        
+        let containsVapor = fileContents.contains(vaporUrl)
+        
+        return containsVapor
+    } catch {
+        return false
+    }
+}
+
+func restoreAccessToFolderOld() {
+    guard let bookmarkData = UserDefaults.standard.data(forKey: udKey) else {
+        return
+    }
+    
+    var isStale = false
+    
+    do {
+        let url = try URL(
+            resolvingBookmarkData: bookmarkData,
+            options: .withSecurityScope,
+            relativeTo: nil,
+            bookmarkDataIsStale: &isStale
+        )
+        
+        projectsFolder = url.path
+        
+        if url.startAccessingSecurityScopedResource() {
+            // You can now access the folder here
+        }
+        
+        if isStale {
+            print("Bookmark data is stale. Need to reselect folder for a new bookmark")
+        }
+    } catch {
+        print("Error restoring access: \(error)")
+    }
+}
 //}
+
+/*private*/ func lastAccessDate(_ path: String) -> Date? {
+    path.withCString {
+        var statStruct = Darwin.stat()
+        
+        guard stat($0, &statStruct) == 0 else {
+            return nil
+        }
+        
+        let interval = TimeInterval(statStruct.st_atimespec.tv_sec)
+        
+        return Date(timeIntervalSince1970: interval)
+    }
+}
--- XProj/New Nav/Models/Recipe.swift
+++ XProj/New Nav/Models/Recipe.swift
@@ -5,7 +5,12 @@ import SwiftUI
 struct Recipe: Decodable, Hashable, Identifiable {
     var id: String
     var name: String
+    let type: ProjType
     var category: Category
+    let openedAt: Date
+    let modifiedAt: Date?
+    let createdAt: Date?
+    
     //    var ingredients: [Ingredient] = []
     //    var related: [Recipe.ID] = []
     //    var imageName: String? = nil
@@ -13,20 +18,34 @@ struct Recipe: Decodable, Hashable, Identifiable {
     init(
         id: String,
         name: String,
-        category: Category
+        type: ProjType,
+        category: Category,
+        openedAt: Date,
+        modifiedAt: Date?,
+        createdAt: Date?
     ) {
         self.id = id
         self.name = name
+        self.type = type
         self.category = category
+        self.openedAt = openedAt
+        self.modifiedAt = modifiedAt
+        self.createdAt = createdAt
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(String.self, forKey: .id)
         
+        id = try container.decode(String.self, forKey: .id)
         name = try container.decode(String.self, forKey: .name)
+        type = try container.decode(ProjType.self, forKey: .type)
         
         category = try container.decode(Category.self, forKey: .category)
+        
+        openedAt = try container.decode(Date.self, forKey: .openedAt)
+        modifiedAt = try container.decodeIfPresent(Date.self, forKey: .modifiedAt)
+        createdAt = try container.decodeIfPresent(Date.self, forKey: .createdAt)
+        
         //        ingredients = try container.decode([Ingredient].self, forKey: .ingredients)
         
         //        let relatedIdStrings = try container.decode([String].self, forKey: .related)
@@ -38,10 +57,11 @@ struct Recipe: Decodable, Hashable, Identifiable {
     private enum CodingKeys: String, CodingKey {
         case id,
              name,
-             category
-        //             ingredients,
-        //             related,
-        //             imageName
+             type,
+             category,
+             openedAt,
+             modifiedAt,
+             createdAt
     }
 }
 
