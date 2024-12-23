0a6b96d752ddef0438412685cca36e7904e55249
TopScrech <sergei_saliukov@icloud.com>
2024-12-23 01:12:39 +0100

    -

--- XProj/funcs/icon.swift
+++ XProj/funcs/icon.swift
@@ -2,10 +2,10 @@ import Foundation
 
 func icon(_ platform: String) -> String {
     switch platform.split(separator: " ").first {
-    case "iOS": "iphone"
-    case "macOS": "macbook"
+    case "iOS":     "iphone"
+    case "macOS":   "macbook"
     case "watchOS": "applewatch"
-    case "tvOS": "tv"
+    case "tvOS":    "tv"
     case "visionOS": "vision.pro"
     default: ""
     }
