51355c0f18078bad8f5bd8cb1ab2d779ac22cc43
TopScrech <sergei_saliukov@icloud.com>
2024-12-24 02:47:22 +0100

    -

--- XProj/Model/App Store App.swift
+++ XProj/Model/App Store App.swift
@@ -14,22 +14,28 @@ struct AppStoreApp: Identifiable, Decodable, Hashable {
 
 extension Target {
     func fetchAppStoreApp() async -> AppStoreApp? {
+        guard let bundleId else {
+            return nil
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(bundleId)")!
+        
         do {
-            guard let bundleId else {
-                return nil
-            }
-            
-            let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(bundleId)")!
             let (data, _) = try await URLSession.shared.data(from: url)
             
-            // Decode JSON into AppStoreAppResponse
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
             let decodedResponse = try decoder.decode(AppStoreAppResponse.self, from: data)
             
-            // Find the first result matching the bundleId
-            if let matchingResult = decodedResponse.results.first(where: { $0.bundleId == bundleId }),
-               let trackViewUrl = URL(string: matchingResult.trackViewUrl) {
+            let matchingResult = decodedResponse.results.first {
+                $0.bundleId == bundleId
+            }
+            
+            if
+                let matchingResult,
+                let trackViewUrl = URL(string: matchingResult.trackViewUrl)
+            {
                 return AppStoreApp(matchingResult.trackName, url: trackViewUrl)
             }
             
