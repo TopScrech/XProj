d27b94b3a70cc3e796267a02ef66c058e8aee0af
TopScrech <sergei_saliukov@icloud.com>
2025-09-07 18:43:45 +0200

    refactoring

--- XProj/Settings/AppSettings.swift
+++ XProj/Settings/AppSettings.swift
@@ -1,11 +1,8 @@
-import ScrechKit
+import SwiftUI
 import LaunchAtLogin
 
 struct AppSettings: View {
     @Environment(NavModel.self) private var nav
-    @Environment(DataModel.self) private var vm
-    @Environment(DerivedDataVM.self) private var ddvm
-    @EnvironmentObject private var store: ValueStore
     
     var body: some View {
         Form {
@@ -21,91 +18,9 @@ struct AppSettings: View {
                 LaunchAtLogin.Toggle()
             }
             
-            Section("Selected folders") {
-                Button {
-                    vm.showPicker()
-                } label: {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 5) {
-                            Label("Projects", systemImage: "folder")
-                            
-                            Text(vm.projectsFolder.isEmpty ? "Not selected" : vm.projectsFolder)
-                                .tertiary()
-                                .lineLimit(2)
-                        }
+            AppSettingsSelectedFolders()
                         
-                        Spacer()
-                        
-                        if !vm.projectsFolder.isEmpty {
-                            SFButton("xmark") {
-                                vm.projectsFolder = ""
-                                deleteBookmark("derived_data_bookmark")
-                                vm.projects = []
-                            }
-                            .foregroundStyle(.red)
-                        }
-                    }
-                    .animation(.default, value: vm.projectsFolder)
-                }
-                
-                Button {
-                    ddvm.showPicker()
-                } label: {
-                    HStack {
-                        VStack(alignment: .leading, spacing: 5) {
-                            Label("Derived Data", systemImage: "folder")
-                            
-                            Text(ddvm.derivedDataUrl?.description ?? "Not selected")
-                                .tertiary()
-                                .lineLimit(2)
-                        }
-                        
-                        Spacer()
-                        
-                        if ddvm.derivedDataUrl?.description != nil {
-                            SFButton("xmark") {
-                                ddvm.derivedDataUrl = nil
-                                deleteBookmark("derived_data_bookmark")
-                                ddvm.folders = []
-                            }
-                            .foregroundStyle(.red)
-                        }
-                    }
-                    .animation(.default, value: ddvm.derivedDataUrl)
-                }
-            }
-            
-            Section("Project details") {
-                Toggle(isOn: $store.showProjTargets) {
-                    Label("Targets", systemImage: "macbook.and.iphone")
-                }
-                
-                if store.showProjTargets {
-                    Toggle(isOn: $store.showProjTargetVersion) {
-                        Label("Target version", systemImage: "info")
-                    }
-                }
-                
-                Toggle(isOn: $store.showProjPackageDependencies) {
-                    Label("Package dependencies", systemImage: "shippingbox")
-                }
-                
-                Toggle(isOn: $store.showProjAppStoreLink) {
-                    Label("App Store link", systemImage: "link")
-                }
-                
-                Toggle(isOn: $store.showGitignore) {
-                    Label("Git ignore", systemImage: "app.connected.to.app.below.fill")
-                }
-                
-                Toggle(isOn: $store.showProjCodeLines) {
-                    Label("Code line count", systemImage: "list.number")
-                }
-                
-                if store.showProjCodeLines {
-                    TextField("File extensions for counting", text: $store.codeLineCountingExtensions)
-                }
-            }
+            AppSettingsProjDetails()
             
             Section("Debug") {
                 Button("Save example projects to Downloads folder", systemImage: "square.and.arrow.down") {
@@ -123,7 +38,6 @@ struct AppSettings: View {
         .buttonStyle(.plain)
         .scrollIndicators(.never)
         .frame(width: 500, height: 600)
-        .animation(.default, value: store.showProjTargets)
     }
     
     private func downloadExamples() {
@@ -155,7 +69,5 @@ struct AppSettings: View {
 #Preview {
     AppSettings()
         .environment(NavModel.shared)
-        .environment(DataModel.shared)
-        .environment(DerivedDataVM())
         .environmentObject(ValueStore())
 }
