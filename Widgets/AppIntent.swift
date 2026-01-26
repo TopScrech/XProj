7d4e2d8ef4a6a1a655a68f453e049e27b5af8745
TopScrech <sergei_saliukov@icloud.com>
2026-01-26 02:51:36 +0100

    #20 recreated the widgets extension

--- Widgets/AppIntent.swift
+++ Widgets/AppIntent.swift
@@ -2,9 +2,10 @@ import WidgetKit
 import AppIntents
 
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
-    static let title: LocalizedStringResource = "Configuration"
-    static let description: IntentDescription = "This is an example widget"
+    static var title: LocalizedStringResource = "Configuration"
+    static var description: IntentDescription = "This is an example widget"
     
+    // An example configurable parameter.
     @Parameter(title: "Favorite Emoji", default: "😃")
     var favoriteEmoji: String
 }
---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+import WidgetKit
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+    }
+}
